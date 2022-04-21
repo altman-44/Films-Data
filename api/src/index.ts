5b9d4ea47c019a70a8b9014876df77dda04d253c
altman-44 <altman.nicolas44@gmail.com>
2022-04-21 10:59:15 -0300

    Fixed path to static files and index.html

--- api/src/index.ts
+++ api/src/index.ts
@@ -20,12 +20,12 @@ const options = {
 };
 // parse data with connect-multiparty. 
 app.use(formData.parse(options));
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, '/../client/build')))
 
 app.use('/films', routes)
 // Si ninguna ruta matchea con las de arriba, va al cliente, al index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../client/build/index.html'))
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'))
 })
 
 app.listen(app.get('PORT'), () => {
